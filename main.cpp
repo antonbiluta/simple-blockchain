892fbd24f6681bac624c1aa0a546c4d645b09a99
antonbiluta <antonbiluta@gmail.com>
2023-09-14 21:23:29 +0300

    #1: Первые шаги

--- main.cpp
+++ main.cpp
@@ -1,6 +1,151 @@
 #include <iostream>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+// Transaction Data
+struct TransactionData {
+    double amount;
+    string senderKey;
+    string receiverKey;
+    time_t timestamp;
+};
+
+// Класс блока
+class Block {
+private:
+    int index;
+    size_t blockHash;
+    size_t previousHash;
+    size_t generateHash();
+
+public:
+    // Constructor
+    Block(int idx, TransactionData d, size_t prevHash);
+
+    // Get Original Hash
+    size_t getHash();
+
+    // Get Previous Hash
+    size_t getPreviousHash();
+
+    // Transaction Data
+    TransactionData data;
+
+    // Validate Hash
+    bool isHashValid();
+};
+
+// Конструктор с некоторыми параметрами
+Block::Block(int idx, TransactionData d, size_t prevHash) {
+    index = idx;
+    data = d;
+    previousHash = prevHash;
+    blockHash = generateHash();
+}
+
+// private functions
+size_t Block::generateHash() {
+    hash<string> hash1;
+    hash<size_t> hash2;
+    hash<size_t> finalHash;
+    string toHash = to_string(data.amount) + data.receiverKey + data.senderKey + to_string(data.timestamp);
+
+    return finalHash(hash1(toHash) + hash2(previousHash));
+}
+
+// public functions
+size_t Block::getHash() {
+    return blockHash;
+}
+
+size_t Block::getPreviousHash() {
+    return previousHash;
+}
+
+bool Block::isHashValid() {
+    return generateHash() == blockHash;
+}
+
+// Блокчеин класс
+class Blockchain {
+private:
+    Block createGenesisBlock();
+
+public:
+    // Public chain
+    vector<Block> chain;
+
+    // Constructor
+    Blockchain();
+
+    // Public Functions
+    void addBlock(TransactionData data);
+    bool isChainValid();
+
+    // Contrived Example For Demo Only!!!!
+    Block *getLatestBlock();
+};
+
+// Конструктор блокчейна
+Blockchain::Blockchain() {
+    Block genesis = createGenesisBlock();
+    chain.push_back(genesis);
+}
+
+Block Blockchain::createGenesisBlock() {
+    time_t current;
+    TransactionData d;
+    d.amount = 0;
+    d.senderKey = "None";
+    d.timestamp = time(&current);
+    hash<int> hash1;
+    Block genesis(0, d, hash1(0));
+    return genesis;
+}
+
+Block *Blockchain::getLatestBlock() {
+    return &chain.back();
+}
+
+void Blockchain::addBlock(TransactionData d) {
+    int index = (int)chain.size() - 1;
+    Block newBlock(index, d, getLatestBlock() -> getHash());
+}
+
+bool Blockchain::isChainValid() {
+    vector<Block>::iterator it;
+    int chainLen = (int)chain.size();
+
+    for(it = chain.begin(); it != chain.end(); ++it) {
+        Block currentBlock = *it;
+        if (!currentBlock.isHashValid())
+            return false;
+
+        if (chainLen > 1) {
+            Block previousBlock = *(it - 1);
+            if (currentBlock.getPreviousHash() != previousBlock.getHash())
+                return false;
+        }
+    }
+    return true;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    // Start blockchain
+    Blockchain AwesomeCoin;
+
+    // Data for first block
+    TransactionData data1;
+    time_t data1Time;
+    data1.amount = 1.5;
+    data1.receiverKey = "Anton";
+    data1.senderKey = "Biluta";
+    data1.timestamp = time(&data1Time);
+
+    AwesomeCoin.addBlock(data1);
+
+    cout << "Is chain valid?" << endl
+        << AwesomeCoin.isChainValid() << endl;
 }
